aec33b846347b70afe4d1aca4cff1c3e2f3de9f0
Aditya Kumar Jha <adityajha126@gmail.com>
2019-07-05 19:23:52 +0530

    started

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+ 
+mongoose.connect(process.env.mongoURI, { useNewUrlParser: true })
+    .then(() => console.log('You are now connected to Mongo!'))
+    .catch(err => console.error('Something went wrong', err))
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {type: Date, default: Date.now},
+  ispublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse(){
+  const course = new Course({
+    name: 'Node Course',
+    author: 'mosh',
+    tags: ['node','backend'],
+    ispublished: true
+  });
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
