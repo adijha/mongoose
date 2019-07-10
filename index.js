29be9833a4e40f4fa24eff8dd0ffacb6143c586f
Aditya Kumar Jha <adityajha126@gmail.com>
2019-07-10 15:42:18 +0530

    querried to the documents

--- index.js
+++ index.js
@@ -26,7 +26,17 @@ async function createCourse(){
   console.log(result);
 }
 //using promices //
-createCourse();
+// createCourse();
+
+//quering to the database
+async function getCourses(){
+  const courses = await Course
+    .find({author: 'mosh', isPublshed: true})
+    .limit(10)
+    .sort({name: 1})
+    .select({ name: 1, tags:1});
+  console.log(courses);
+}
+getCourses();
 
 
-//demo
